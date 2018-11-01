d9631674076efb451aa33a40678caaf9f1be94f1
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-01 12:11:45 -0600

    added express function to display survey.html

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const path = require("path");
 const express = require("express");
+
+const app = express();
+const PORT = 3050;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/survey", function (req, res) {
+
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
