2dd259009ad1cb84c733171fdb28650edf4adfb6
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-01 13:41:13 -0600

    added necessary elements and basic design to home.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,23 @@
+const path = require("path");
+const express = require("express");
 
+const app = express();
+const PORT = 3050;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const ModAPI = function () {
+
+    this.dispAPI = function () {
+
+        app.get("/api/friends", function (req, res) {
+
+            res.sendFile(path.join(__dirname, "../public/survey.html"));
+
+        });
+
+    },
+
+}
 
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// const path = require("path");
-// const express = require("express");
 const Display = require("./app/routing/htmlRoutes.js");
 
 let htmlRoutes = new Display();
