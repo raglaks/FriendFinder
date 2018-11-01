18938dd0e06a81dffb618a7e5b6079af34bb8b70
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-01 13:13:45 -0600

    code refactored in htmlRoutes.js so that when server runs, the displays work

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,21 +1,39 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = 3050;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const Display = function () {
 
-    this.dispSurvey = function() {
+    this.dispSurvey = function () {
 
         app.get("/survey", function (req, res) {
 
-            res.sendFile(path.join(__dirname, "./app/public/survey.html"));
-    
+            res.sendFile(path.join(__dirname, "../public/survey.html"));
+
         });
 
     },
 
-    this.dispElse = function() {
+    this.dispElse = function () {
 
         app.get("*", function (req, res) {
 
-            res.sendFile(path.join(__dirname, "./app/public/home.html"));
-    
+            res.sendFile(path.join(__dirname, "../public/home.html"));
+
+        });
+
+    },
+
+    this.listenDisp = function () {
+
+        app.listen(PORT, function () {
+
+            console.log("App listening on PORT " + PORT);
         });
 
     }
--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-const path = require("path");
-const express = require("express");
+// const path = require("path");
+// const express = require("express");
+const Display = require("./app/routing/htmlRoutes.js");
 
-const app = express();
-const PORT = 3050;
+let htmlRoutes = new Display();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+htmlRoutes.dispSurvey();
+htmlRoutes.dispElse();
+htmlRoutes.listenDisp();
 
-app.get("/survey", function (req, res) {
-
-    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
-
-});
-
-app.get("*", function (req, res) {
-
-    res.sendFile(path.join(__dirname, "./app/public/home.html"));
-
-});
-
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
