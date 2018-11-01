bff09486d1783ec9603b10c495100b49893c1ca9
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-01 12:16:37 -0600

    added function to display home page regardless of route (except for /survey)

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get("/survey", function (req, res) {
 
 });
 
+app.get("*", function (req, res) {
+
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+
+});
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
