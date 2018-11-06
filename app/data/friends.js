420644d85463b706e4be9542b9e1cbe00fa6b9f4
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-06 09:14:13 -0600

    added readfile function to friends.js to acquire data from API and start writing comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,25 @@
-const resArray = [];
+const fs = require("file-system");
 
-module.exports = resArray;
+function readAPI() {
+
+    fs.readFile(path.join(__dirname, "../data/friends.txt"), "utf8", function (err, data) {
+
+        console.log(data);
+
+        if (err) throw err;
+
+        if (data) {
+
+            console.log(JSON.parse(data));
+
+        } else {
+
+            console.log("input data--API is empty");
+
+        }
+
+    });
+
+}
+
+readAPI();
